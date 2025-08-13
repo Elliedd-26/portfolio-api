59b183425ddd188bb184bcb2f51008ab9e253620
Qianyi <n01718099@humber.ca>
2025-08-13 12:16:59 -0400

    Add CORS support for React frontend

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors"); // ADD THIS - for allowing React to connect
 
 const adminRoutes = require("./routes/adminRoutes");
 const apiRoutes = require("./routes/apiRoutes");
@@ -14,9 +15,11 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 // middleware
+app.use(cors()); // ADD THIS - allows React app to make requests
 app.set("view engine", "pug");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // ADD THIS - for handling JSON requests from React
 
 // routes
 app.use("/admin", adminRoutes);
